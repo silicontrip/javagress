39846cf645efbc1136b214ebcf1ce3cd1a9c9282
Mark Heath <silicontrip@gmail.com>
2016-09-01 11:18:26 +1000

    Changes for MU Estimation.

--- runner.sh
+++ runner.sh
@@ -4,4 +4,4 @@ mainclass=$( basename $0 | sed s/.sh$//)
 
 echo $mainclass
 
-java -cp jackson-annotations-2.3.3.jar:jackson-core-2.3.4.jar:jackson-databind-2.3.4.jar:portaltools.jar $mainclass "$@"
+java -cp guava-18.0.jar:s2-geometry-java.jar:jackson-annotations-2.3.3.jar:jackson-core-2.3.4.jar:jackson-databind-2.3.4.jar:portaltools.jar $mainclass "$@"
