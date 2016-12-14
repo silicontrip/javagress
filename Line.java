ac56f7f5230f14eb4d77d72a83a4d07a30646f35
mark heath <mjpeg0@silicontrip.org>
2016-12-14 21:06:18 +1100

    re-indent

--- Line.java
+++ Line.java
@@ -1,6 +1,6 @@
 import javax.vecmath.Vector3d;
 public class Line {
-
+	
 	Point o;
 	Point d;
 	
@@ -8,12 +8,12 @@ public class Line {
 	
 	// close to zero threshold
 	final static  double eps = 1E-10;
-
+	
 	public Long getdLat() { return d.getLatE6(); }
 	public Long getdLng() { return d.getLngE6(); }
 	//public void setdLat(Long l) { d.setLat(l); }
 	//public void setdLng(Long l) { d.setLng(l); }
-
+	
 	public Long getoLat() { return o.getLatE6(); }
 	public Long getoLng() { return o.getLngE6(); }
 	
@@ -21,25 +21,25 @@ public class Line {
 	public Double getoLngAsDouble() { return new Double(o.getLngE6()); }
 	public Double getdLatAsDouble() { return new Double(d.getLatE6()); }
 	public Double getdLngAsDouble() { return new Double(d.getLngE6()); }
-
+	
 	public Vector3d getoVect() { return new Vector3d(getoX(),getoY(),getoZ()); }
 	public Vector3d getdVect() { return new Vector3d(getdX(),getdY(),getdZ()); }
 	
 	public double getoX() { return Math.cos(Math.toRadians(o.getLat())) * Math.cos(Math.toRadians(o.getLng())); }
 	public double getoY() { return Math.cos(Math.toRadians(o.getLat())) * Math.sin(Math.toRadians(o.getLng())); }
 	public double getoZ() { return Math.sin(Math.toRadians(o.getLat())); }
-
+	
 	public double getdX() { return Math.cos(Math.toRadians(d.getLat())) * Math.cos(Math.toRadians(d.getLng())); }
 	public double getdY() { return Math.cos(Math.toRadians(d.getLat())) * Math.sin(Math.toRadians(d.getLng())); }
 	public double getdZ() { return Math.sin(Math.toRadians(d.getLat())); }
 	
 	//public void setoLat(Long l) { o.setLat(l); }
 	//public void setoLng(Long l) { o.setLng(l); }
-
+	
 	public Point getD() { return d; }
 	public Point getO() { return o; }
-
-	public Line (Point d, Point o) 
+	
+	public Line (Point d, Point o)
 	{
 		this.d = new Point(d);
 		this.o = new Point(o);
@@ -48,9 +48,9 @@ public class Line {
 		
 		//this.d = d;
 		//this.o = o;
-
+		
 	}
-
+	
 	public Line (Long dla,Long dlo, Long ola,Long olo)
 	{
 		this.d = new Point(dla,dlo);
@@ -60,10 +60,10 @@ public class Line {
 	@Override
 	public final boolean equals(Object obj2)
 	{
-  if (obj2 == this) return true;
-  if (!(obj2 instanceof Line)) return false;
-  Line l = (Line) obj2;
-
+  		if (obj2 == this) return true;
+  		if (!(obj2 instanceof Line)) return false;
+  		Line l = (Line) obj2;
+		
 		return (this.getO().equals(l.getO()) && this.getD().equals(l.getD())) || (this.getO().equals(l.getD()) && this.getD().equals(l.getO()));
 	}
 	
@@ -73,7 +73,7 @@ public class Line {
 		return getO().hashCode() | getD().hashCode();
 	}
 	
-	public boolean equals(Line[] lines)
+	public boolean findIn(Line[] lines)
 	{
 		for (Line line : lines)
 		{
@@ -86,91 +86,91 @@ public class Line {
 	
 	public int greaterCircleIntersectType (Line l)
 	{
-
+		
 		Vector3d p0 = this.getoVect();
 		Vector3d p1 = this.getdVect();
 		Vector3d p2 = l.getoVect();
 		Vector3d p3 = l.getdVect();
-
+		
 		Vector3d V = new Vector3d();
 		V.cross(p0,p1);
 		V.normalize();
-
+		
 		Vector3d U = new Vector3d();
 		U.cross(p2,p3);
 		U.normalize();
-
+		
 		Vector3d D = new Vector3d();
 		D.cross( V, U );
 		if (D.length() == 0)
 			return 0; // equal
 		D.normalize();
 		
-				
+		
 		Vector3d S1 = new Vector3d();
 		Vector3d S2 = new Vector3d();
 		Vector3d S3 = new Vector3d();
 		Vector3d S4 = new Vector3d();
-
+		
 		S1.cross(p0,V);
 		S2.cross(p1,V);
 		S3.cross(p2,U);
 		S4.cross(p3,U);
-
+		
 		double s0 = -S1.dot(D);
 		double s1 = S2.dot(D);
 		double s2 = -S3.dot(D);
 		double s3 = S4.dot(D);
-
+		
 		//System.out.println("Signs: " + s0 + " " + s1 + " " + s2 + " " + s3 );
-
+		
 		int count=0,zero=0;
-
-		if (Math.abs(s0) < eps) 
-			zero++; 
-		else 
+		
+		if (Math.abs(s0) < eps)
+			zero++;
+		else
 			count += Math.signum(s0);
-		if (Math.abs(s1) < eps) 
-			zero++; 	
-		else 
+		if (Math.abs(s1) < eps)
+			zero++;
+		else
 			count += Math.signum(s1);
-		if (Math.abs(s2) < eps) 
-			zero++; 
-		else 
+		if (Math.abs(s2) < eps)
+			zero++;
+		else
 			count += Math.signum(s2);
-
-		if (Math.abs(s3) < eps) 
-			zero++; 
-		else 
+		
+		if (Math.abs(s3) < eps)
+			zero++;
+		else
 			count += Math.signum(s3);
-
+		
 		if (count == -4 || count == 4)
 			return 1;
-
+		
 		if (zero==4)
 			return 0;
-
+		
 		if (zero > 0)
 			return 3;
-
+		
 		return 2;
-
+		
 	}
-	public Boolean intersects(Line l) { 
-
+	public Boolean intersects(Line l) {
+		
 		// DrawTools dt = new DrawTools();
 		//dt.addLine(this);
 		//dt.addLine(l);
-
+		
 		//int i = intersectType(l);
 		int gi = greaterCircleIntersectType(l);
-
+		
 		// really would like some unit tests now.
 		return (gi == 1);
 	}
 	public Boolean intersectsOrEqual(Line l) { return (greaterCircleIntersectType(l) != 2);	}
 	public Boolean equalLine(Line l) { return (greaterCircleIntersectType(l) == 0);	}
-
+	
 	public boolean intersects (Line[] lines)
 	{
 		
@@ -183,7 +183,7 @@ public class Line {
 	}
 	
 	public boolean intersectsOrEquals (Line[] lines)
-	{		
+	{
 		for (Line line : lines)
 		{
 			if (intersectsOrEqual(line)) { return true; }
@@ -198,7 +198,7 @@ public class Line {
 	
 	public String toString() {
 		
-		return new String ( "{\"type\":\"polyline\",\"latLngs\":[{\"lat\":" + this.getoLat()/1000000.0 + 
+		return new String ( "{\"type\":\"polyline\",\"latLngs\":[{\"lat\":" + this.getoLat()/1000000.0 +
 						   ",\"lng\":" + this.getoLng()/1000000.0 +
 						   "},{\"lat\":" + this.getdLat()/1000000.0 +
 						   ",\"lng\":" + this.getdLng()/1000000.0 +
@@ -207,7 +207,7 @@ public class Line {
 		// return new String ( getoLatAsDouble() + "," + getoLngAsDouble() + " - " + getdLatAsDouble() +","+ getdLngAsDouble());
 		
 	}
-
+	
 	public Double getBearing() { return o.getBearingTo(d); }
 	public Double getReverseBearing() { return d.getBearingTo(o); }
 	
