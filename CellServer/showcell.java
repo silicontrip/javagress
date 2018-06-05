45b993d6db25af649cbfb3b5e20f79d81193e44f
Mark Heath <silicontrip@gmail.com>
2018-06-05 14:15:26 +1000

    Searches for requested field in MongoDB.

--- CellServer/showcell.java
+++ CellServer/showcell.java
@@ -11,6 +11,10 @@ import java.util.Iterator;
 import org.json.*;
 import com.google.common.geometry.*;
 
+import org.bson.*;
+import com.mongodb.*;
+import com.mongodb.client.*;
+
 public class showcell
 {
 
@@ -18,13 +22,19 @@ public class showcell
     {
         try
         {
-		BufferedReader reader = new BufferedReader(new FileReader("res50.json"));
-		String line = reader.readLine();
-		JSONObject mudb = new JSONObject(line);
-		reader.close();
-		 	CellServer cs = new CellServer(mudb);
+
+		MongoClient mongo;
+		MongoDatabase db;
+		MongoCursor<Document> cursor;
+		MongoCollection<org.bson.Document> table;
+
+		mongo = new MongoClient("localhost", 27017);
+		db = mongo.getDatabase("ingressmu");
+		table = db.getCollection("ingressmu");
+
+		CellServer cs = new CellServer();
                 
-                    JSONObject response = new JSONObject();
+		JSONObject response = new JSONObject();
 
 		for(String data: args) {
 			JSONArray dtobj = new JSONArray(data); 
@@ -36,26 +46,89 @@ public class showcell
                         }
                         else
                         {
+				UniformDistribution totalmu = new UniformDistribution(0,0);
 				for (int i=0; i < dtobj.length(); i++)
 				{
 					response = cs.cellalize(dtobj.getJSONObject(i));
 
-			    System.out.println( "" + response + "\n\r" );
+					JSONArray points = dtobj.getJSONObject(i).getJSONArray("latLngs");
+
+					BasicDBObject pointA = new BasicDBObject();
+					BasicDBObject pointB = new BasicDBObject();
+					BasicDBObject pointC = new BasicDBObject();
+					long latE6,lngE6;
+					latE6 =(long)(points.getJSONObject(0).getDouble("lat") * 1000000);
+					lngE6 = (long)(points.getJSONObject(0).getDouble("lng") * 1000000);
+					pointA.put("latE6", latE6);
+					pointA.put("lngE6", lngE6);
+					BasicDBObject elemA = new BasicDBObject();
+					elemA.put("$elemMatch",pointA);
+					pointB.put("latE6", (long)(points.getJSONObject(1).getDouble("lat") * 1000000.0));
+					pointB.put("lngE6", (long)(points.getJSONObject(1).getDouble("lng") * 1000000.0));
+					BasicDBObject elemB = new BasicDBObject();
+					elemB.put("$elemMatch",pointB);
+					pointC.put("latE6", (long)(points.getJSONObject(2).getDouble("lat") * 1000000.0));
+					pointC.put("lngE6", (long)(points.getJSONObject(2).getDouble("lng") * 1000000.0));
+					BasicDBObject elemC = new BasicDBObject();
+					elemC.put("$elemMatch",pointC);
+
+					BasicDBList pointlist = new BasicDBList();
+
+					pointlist.add(elemA);   
+					pointlist.add(elemB);   
+					pointlist.add(elemC);   
+
+					BasicDBObject allpoints = new BasicDBObject();
+					allpoints.put("$all",pointlist);
+                
+					BasicDBObject findpoints = new BasicDBObject();
+					findpoints.put("data.points",allpoints);
+
+					//System.out.println(findpoints);
+					System.out.println("found: " + table.count(findpoints));
+					cursor = table.find(findpoints).iterator();
+
+
+                while (cursor.hasNext()) {
+                        Document entitycontent = cursor.next();
+                        System.out.println(entitycontent);
+                }
+
+
+		//	    System.out.println( "" + response.toString(4) + "\n\r" );
+			UniformDistribution fieldmu = new UniformDistribution(0,0);
 			double min_mu = 0;
 			double max_mu = 0;
 			for (Iterator<String> id= response.keys(); id.hasNext();)
 			{
-				JSONObject cell = response.getJSONObject(id.next());
+				String cellid = id.next();
+				JSONObject cell = response.getJSONObject(cellid);
 				//System.out.println(cell);
-				min_mu += cell.getDouble("area") * cell.getDouble("mu_min");
-				max_mu += cell.getDouble("area") * cell.getDouble("mu_max");
+				if (cell.has("mu_min"))
+				{
+					UniformDistribution cellmu = new UniformDistribution(cell.getDouble("mu_min"),cell.getDouble("mu_max"));
+					System.out.print("" + cellid + ": " + cellmu + " x " + cell.getDouble("area") + " = ");
+					cellmu = cellmu.mul(cell.getDouble("area"));
+					//double cmin_mu = cell.getDouble("area") * cell.getDouble("mu_min");
+					//double cmax_mu = cell.getDouble("area") * cell.getDouble("mu_max");
+					//System.out.print("field: " + fieldmu + " + ");
+					System.out.println("" + cellmu);
+					fieldmu = fieldmu.add(cellmu);
+					//System.out.println(" =  " + fieldmu);
+					//min_mu += cmin_mu;
+					//max_mu += cmax_mu;
+				} else {
+					System.out.println("No Information for cell: " + cellid);
+				}
 			}
-			System.out.println ("MU: [" + min_mu + "," + max_mu + "] " + ((min_mu + max_mu) / 2 ) + " +/- " + (max_mu - min_mu) / (max_mu+min_mu) * 100 + "%");
+			System.out.println ("MU: "  + fieldmu+ " " + fieldmu.mean() + " +/- " + fieldmu.perror() * 100 + "%");
+			totalmu = totalmu.add(fieldmu);
 			}
+			System.out.println ("TotalMU: "  + totalmu+ " " + totalmu.mean() + " +/- " + totalmu.perror() * 100 + "%");
                         }
 		}
         }
-        catch ( IOException e )
+        catch ( Exception e )
         {
             System.out.println( " Error: " + e.toString() );
         }
