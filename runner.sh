c4b562dcc574353845431cee947602957c00d1f3
Mark Heath <silicontrip@gmail.com>
2019-01-15 13:37:16 +1100

    merge seperate repositories

--- runner.sh
+++ runner.sh
@@ -4,4 +4,4 @@ mainclass=$( basename $0 | sed s/.sh$//)
 
 echo $mainclass
 
-java -cp CellServer/cellserver.jar:vecmath-1.5.1.jar:guava-18.0.jar:s2-geometry-java.jar:jackson-annotations-2.3.3.jar:jackson-core-2.3.4.jar:jackson-databind-2.3.4.jar:portaltools.jar $mainclass "$@"
+java -cp cellserver.jar:vecmath-1.5.1.jar:guava-18.0.jar:s2-geometry-java.jar:jackson-annotations-2.9.6.jar:jackson-core-2.9.6.jar:jackson-databind-2.9.6.jar:portaltools.jar $mainclass "$@"
